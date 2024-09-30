9c1973828709e97bf222e189b5a8fc23515554fe
1Aksoni <akshat2001soni@gmail.com>
2024-09-30 22:53:10 +0530

    new vite file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "https://hevenhubbackend.onrender.com" // Update this line
+      '/api': {
+        target: 'https://hevenhubbackend.onrender.com',
+        changeOrigin: true,  // Needed for virtual hosted sites
+        secure: false, // If you're using HTTP
+      },
     },
   },
   plugins: [react()],
-})
+});
